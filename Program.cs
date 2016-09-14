b6f53630c8243314cf1d2d96d2fbaf56915c8e8c
WilliamIV <william.green.iv@gmail.com>
2016-09-14 09:47:14 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuesSlide37HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter and integer.");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n % 3 != 0)
+
+
+                {
+
+                    Console.WriteLine("You Lost.  Enter another integer.");
+
+
+
+                }
+
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("You win!");
+
+                }
+
+            }
+
+            
+
+        }
+    }
+}
